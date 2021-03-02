6cab18089a38323ccd5229e6c2441f4e6a23287d
brian lai <brian.lai.1992@gmail.com>
2021-03-01 21:37:31 -0500

    more setup stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+window.jQuery = $;
+window.$ = $;
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('packs/jquery')
 
-//= require jquery
-//= require materialize-sprockets
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +19,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import '../stylesheets/application.scss';
